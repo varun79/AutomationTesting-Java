04b6ded83ab146711ae77d5a42814af69d032fd4
varun79 <vpsonavane@gmail.com>
2024-07-07 19:22:08 +0530

    Lab_003 file added

---
+++ src/ex_07072024/Lab_003.java
@@ -0,0 +1,18 @@
+package ex_07072024;
+
+public class Lab_003 {
+    public static void main(String[] args) {
+       // System.out.println();
+
+        //2*2;
+
+        // this is a single comment will not run and execute
+
+        /**
+         * Multiple comments used for Documentation Purpose
+         *
+         *
+         */
+
+    }
+}
